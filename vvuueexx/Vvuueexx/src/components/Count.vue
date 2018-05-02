eeea2799843c9ed27d451d24a2dd63f64d8ba413
liulian0519 <1223342263@qq.com>
2018-05-02 20:22:38 +0800

    vuex store

---
+++ vvuueexx/Vvuueexx/src/components/Count.vue
@@ -0,0 +1,27 @@
+<template>
+    <div>
+      <h2>{{msg}}</h2><hr>
+      <h3>{{$store.state.count}}</h3>
+      <p>
+        <button @click="$store.commit('add')">增加</button>
+        <button @click="$store.commit('reduce')">减少</button>
+      </p>
+    </div>
+</template>
+
+<script>
+  import store from '@/vuex/store'
+    export default {
+        name: "Count",
+      data(){
+          return{
+            msg:'Hello Vuex'
+          }
+      },
+      store
+    }
+</script>
+
+<style scoped>
+
+</style>
