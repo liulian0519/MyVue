2f950eee555a1d9673571846d5f58234dc3edda5
liulian0519 <1223342263@qq.com>
2018-05-04 08:36:03 +0800

    element

--- amzingPOS/src/components/page/Pos.vue
+++ amzingPOS/src/components/page/Pos.vue
@@ -1,17 +1,30 @@
 <template>
-    <div>
-      <h2>
-        <!--welcome to 小潼关！what can I do for you?-->
-      </h2>
+    <div class="pos">
+      <el-row>
+        <el-col :span="7" class="pos-order" id="order-list">
+          我是订单蓝
+        </el-col>
+        <el-col>
+          我是产品蓝
+        </el-col>
+      </el-row>
     </div>
 </template>
 
 <script>
     export default {
-        name: "Pos"
+        name: "Pos",
+      mounted:function () {
+        var orderhight = document.body.clientHeight;
+        console.log(orderhight);
+        document.getElementById('order-list').style.height=orderhight+'px';
+      }
     }
 </script>
 
 <style scoped>
-
+.pos-order{
+  background-color:#f9fafc;
+  border-right: 1px solid #C0CCDA;
+}
 </style>
