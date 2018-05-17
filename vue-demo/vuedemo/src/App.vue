dd640b00db6401ae036e32934d19e85b03820674
liulian0519 <1223342263@qq.com>
2018-05-17 18:05:04 +0800

    new

---
+++ vue-demo/vuedemo/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+
+    <comA @my-event="onCon"></comA>
+    <!--<img src="./assets/logo.png">-->
+    <!--<router-view/>-->
+
+  </div>
+</template>
+
+<script>
+  import comA from '@/components/a'
+export default {
+  name: 'App',
+  components:{
+    comA
+  },
+  methods:{
+    onCon(parmfromA){
+      console.log("fgjknfdklgmfkg"+parmfromA)
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ vue-demo/vuedemo/src/components/a.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+  {{hello}}
+  <button @click="emitdk">dd</button>
+</div>
+</template>
+
+<script>
+    export default {
+        name: "a.vue",
+      data(){
+        return{
+          hello :'I am component A '
+        }
+      },
+      methods:{
+        emitdk(){
+          this.$emit('my-event',this.hello)
+        }
+      },
+      components:{
+
+      }
+    }
+</script>
+
+<style scoped>
+
+</style>
